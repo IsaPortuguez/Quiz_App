b892b29c34a023a57d2f79757c11d29e16b93778
IsaPortuguez <isabelportuguez.dev@gmail.com>
2026-07-08 09:46:54 -0600

    Fix Widget Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,17 +8,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: StartScreen(),
-        ),
-      ),
+      const Quiz(),
     );
 
     // Verify that our counter starts at 0.
