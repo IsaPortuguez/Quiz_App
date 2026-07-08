043a0b6bc47d69ca8318ab7a39312f7618ab6dba
IsaPortuguez <isabelportuguez.dev@gmail.com>
2026-07-08 15:53:37 -0600

    Add Question Model and shuffled function.

---
+++ lib/models/quiz_question.dart
@@ -0,0 +1,12 @@
+class QuizQuestion {
+  const QuizQuestion(this.text, this.answers);
+
+  final String text;
+  final List<String> answers;
+
+  List<String> getShuffledAnswers(){
+    final  shufledList = List.of(answers);
+    shufledList.shuffle();
+    return shufledList;
+  }
+}
