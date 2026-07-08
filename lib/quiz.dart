66069e20748200c789fc690073e83d219441cef1
IsaPortuguez <isabelportuguez.dev@gmail.com>
2026-07-08 09:53:31 -0600

    Refactor: simplify screen state management.

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -13,11 +13,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen();
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -36,7 +36,9 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : QuestionsScreen(),
         ),
       ),
     );
